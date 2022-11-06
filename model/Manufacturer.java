850edf2aba8afa2caba9e2655d48203d536c3fd9
Petar Vasev <35383291+pero909@users.noreply.github.com>
2022-11-06 21:26:23 +0100

    Create Manufacturer.java

---
+++ model/Manufacturer.java
@@ -0,0 +1,49 @@
+
+import lombok.Data;
+
+@Data
+public class Manufacturer {
+    private Long id;
+    private String name;
+    private String country;
+    private String address;
+
+    public Manufacturer(String name, String country, String address) {
+        this.name = name;
+        this.country = country;
+        this.address = address;
+        this.id=(long) (Math.random() * 1000);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
