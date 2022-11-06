657c573b7edb9d08af64797186fe04254b68acdd
Petar Vasev <35383291+pero909@users.noreply.github.com>
2022-11-06 21:32:42 +0100

    Update BallonServiceImp.java

--- service/imp/BallonServiceImp.java
+++ service/imp/BallonServiceImp.java
@@ -1,6 +1,7 @@
 
 import mk.finki.ukim.mk.lab.bootstrap.DataHolder;
 import mk.finki.ukim.mk.lab.model.Balloon;
+import mk.finki.ukim.mk.lab.model.Manufacturer;
 import mk.finki.ukim.mk.lab.repository.BalloonRepository;
 import mk.finki.ukim.mk.lab.service.BaloonService;
 import org.springframework.stereotype.Service;
@@ -23,5 +24,25 @@ public class BallonServiceImp implements BaloonService {
 
     public Optional<Balloon> searchByNameOrDescription(String text) {
         return balloonRepository.findAllByNameOrDescription(text);
+    }
+
+    @Override
+    public Optional<Balloon> searchByNameAndDescription(String name, String desc) {
+        return balloonRepository.findAllByNameAndDescription(name,desc);
+    }
+
+    @Override
+    public Optional<Balloon> findById(Long id) {
+        return balloonRepository.findById(id);
+    }
+
+    @Override
+    public Optional<Balloon> save(String name,Long id, String description, Manufacturer manufacturer) {
+        return balloonRepository.save(name,id,description,manufacturer);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+       this.balloonRepository.deleteById(id);
     }
 }
