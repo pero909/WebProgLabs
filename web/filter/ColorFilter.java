70113d94864bde480bbe91cdfa98fadbb6baa330
Petar Vasev <35383291+pero909@users.noreply.github.com>
2022-11-06 21:38:30 +0100

    Create ColorFilter.java

---
+++ web/filter/ColorFilter.java
@@ -0,0 +1,41 @@
+
+import mk.finki.ukim.mk.lab.model.Order;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter
+public class ColorFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest request=(HttpServletRequest) servletRequest;
+        HttpServletResponse response=(HttpServletResponse) servletResponse;
+
+     //  String color= (String) request.getParameter("color");
+       String path= request.getServletPath();
+       String color = (String) request.getSession().getAttribute("balloonColor");
+
+       if(!"".equals(path)&&color==null&&!"/balloons".equals(path)){
+           response.sendRedirect("");
+       }else{
+           filterChain.doFilter(servletRequest,servletResponse);
+       }
+
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
