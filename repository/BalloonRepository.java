1d2b249081192974d365e3c848366bb9bba492c5
Petar Vasev <35383291+pero909@users.noreply.github.com>
2022-11-06 21:29:30 +0100

    Update BalloonRepository.java

--- repository/BalloonRepository.java
+++ repository/BalloonRepository.java
@@ -1,5 +1,6 @@
 import mk.finki.ukim.mk.lab.bootstrap.DataHolder;
 import mk.finki.ukim.mk.lab.model.Balloon;
+import mk.finki.ukim.mk.lab.model.Manufacturer;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,5 +17,26 @@ public class BalloonRepository {
                 balloon.getName().equals(text)
                         || balloon.getDescription().equals(text))
                 .findFirst();
+    }
+    public Optional<Balloon> findAllByNameAndDescription(String name,String desc){
+        return DataHolder.balloonList.stream().filter(balloon ->
+                        balloon.getName().equals(name)
+                                && balloon.getDescription().equals(desc))
+                .findFirst();
+    }
+    public Optional<Balloon> findById(Long id){
+        return DataHolder.balloonList
+                .stream()
+                .filter(balloon -> balloon.getId().equals(id))
+                .findFirst();
+    }
+    public Optional<Balloon> save(String name, Long id,String description, Manufacturer manufacturer){
+        DataHolder.balloonList.removeIf(i->i.getId().equals(id));
+        Balloon balloon=new Balloon(name,description,manufacturer);
+        DataHolder.balloonList.add(balloon);
+        return Optional.of(balloon);
+    }
+    public void deleteById(Long id){
+        DataHolder.balloonList.removeIf(i->i.getId().equals(id));
     }
 }
