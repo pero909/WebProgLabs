84d8d36161f82429949871cd75dd30786a0f8e3c
Petar Vasev <35383291+pero909@users.noreply.github.com>
2022-11-06 21:32:19 +0100

    Update OrderService.java

--- service/OrderService.java
+++ service/OrderService.java
@@ -1,8 +1,13 @@
-
 import mk.finki.ukim.mk.lab.model.Exceptions.ColorNotChosenException;
 import mk.finki.ukim.mk.lab.model.Order;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface OrderService {
-    Order placeOrder(String balloonColor, String clientName, String address);
-    Order placeOrder(String balloonColor) throws ColorNotChosenException;
+    Order placeOrder(String balloonColor,String balloonSize, String clientName, String address);
+    Optional<Order> findById(Long id);
+    Optional<Order> save(String balloonColor,String balloonSize
+            ,String clientName,String clientAdress,Long orderId);
+    List<Order> findAll();
 }
