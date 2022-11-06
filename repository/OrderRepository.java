c0425362eccffcedf8af96793d05ffe4ed2e60e3
Petar Vasev <35383291+pero909@users.noreply.github.com>
2022-11-06 21:30:39 +0100

    Update OrderRepository.java

--- repository/OrderRepository.java
+++ repository/OrderRepository.java
@@ -1,8 +1,10 @@
+
 import mk.finki.ukim.mk.lab.bootstrap.DataHolder;
 import mk.finki.ukim.mk.lab.model.Order;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class OrderRepository {
@@ -15,5 +17,19 @@ public class OrderRepository {
         }
         DataHolder.orderList.add(c);
         return c;
+    }
+    public Optional<Order> findById(Long id){
+        return DataHolder.orderList
+                .stream()
+                .filter(i->i.getOrderId().equals(id))
+                .findFirst();
+    }
+    public Optional<Order> save(String balloonColor,String balloonSize
+                                ,String clientName,String clientAdress,Long orderId){
+        DataHolder.orderList.removeIf(i->i.getOrderId().equals(orderId));
+        Order order= new Order(balloonColor,balloonSize,clientName,clientAdress);
+        DataHolder.orderList.add(order);
+        return Optional.of(order);
+
     }
 }
